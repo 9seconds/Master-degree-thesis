1d299042b237b2125a2c834c8ab6269f5720ef04
Serge Arkhipov <serge@aerialsounds.org>
2011-05-23 11:13:11 +0400

    Added generation scripts. Need to be checked

--- generate_paper.sh
+++ generate_paper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 MAINFILE="main"
-DOCNAME="masterdegree"
+DOCNAME="paper"
 
 cd Paper
+
 pdflatex ${MAINFILE} > /dev/null && rm -rf ${MAINFILE}.pdf
 pdflatex ${MAINFILE} > /dev/null && mv -f ${MAINFILE}.pdf ../${DOCNAME}.pdf
+
+rm -rf *.{dvi,log,aux,thm,toc}
---
+++ generate_presentation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MAINFILE="main"
+HANDOUTFILE="handout_"
+DOCMAIN="presentation"
+DOCHANDOUT="${DOCMAIN}_handout"
+
+cd Presentation
+
+pdflatex ${MAINFILE} > /dev/null && rm -rf ${MAINFILE}.pdf
+pdflatex ${MAINFILE} > /dev/null && mv -f  ${MAINFILE}.pdf ../${DOCMAIN}.pdf
+
+cat ${MAINFILE}.tex                                                           \
+    | sed -e 's/\\usetheme{Singapore}//'                                      \
+    | sed -e 's/%\\usetheme/\\usetheme/; s/%\\usecolortheme/\\usecolortheme/' \
+        > ${HANDOUTFILE}.tex
+
+pdflatex ${HANDOUTFILE} > /dev/null && rm -rf ${HANDOUTFILE}.pdf
+pdflatex ${HANDOUTFILE} > /dev/null && mv -f  ${HANDOUTFILE}.pdf ../${DOCHANDOUT}.pdf
+
+rm -rf ${HANDOUTFILE}.tex *.{log,aux,dvi,thm,out,snm,toc,nav}
