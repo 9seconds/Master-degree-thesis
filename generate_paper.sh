30dda22fe27672969ce237516280e3a7f6ea65aa
Serge Arkhipov <serge@aerialsounds.org>
2011-05-25 22:16:07 +0400

    PDF size optimizations

--- generate_paper.sh
+++ generate_paper.sh
@@ -3,11 +3,18 @@
 MAINFILE="main"
 DOCNAME="paper"
 
+PDFVERSION=5
+PDFCOMPRESS=9
+PDFOBJCOMPRESS=3
+
+GENOPTS="\pdfminorversion${PDFVERSION}\pdfcompresslevel${PDFCOMPRESS}\pdfobjcompresslevel${PDFOBJCOMPRESS}\input"
+PDFTEX="`which pdflatex` ${GENOPTS}"
+
 cd Paper
 
 echo "- Generating ${DOCNAME}.pdf"
-pdflatex ${MAINFILE} > /dev/null && rm -rf ${MAINFILE}.pdf
-pdflatex ${MAINFILE} > /dev/null && mv -f ${MAINFILE}.pdf ../${DOCNAME}.pdf
+${PDFTEX} ${GENOPTS} ${MAINFILE} > /dev/null && rm -f ${MAINFILE}.pdf
+${PDFTEX} ${GENOPTS} ${MAINFILE} > /dev/null && mv -f ${MAINFILE}.pdf ../${DOCNAME}.pdf
 
 echo "+ Cleaning temporary files"
 rm -rf *.{dvi,log,aux,thm,toc,out}
--- generate_presentation.sh
+++ generate_presentation.sh
@@ -5,11 +5,18 @@ HANDOUTFILE="handout_"
 DOCMAIN="presentation"
 DOCHANDOUT="${DOCMAIN}_handout"
 
+PDFVERSION=5
+PDFCOMPRESS=9
+PDFOBJCOMPRESS=3
+
+GENOPTS="\pdfminorversion${PDFVERSION}\pdfcompresslevel${PDFCOMPRESS}\pdfobjcompresslevel${PDFOBJCOMPRESS}\input"
+PDFTEX="`which pdflatex` ${GENOPTS}"
+
 cd Presentation
 
 echo "- Generating ${DOCMAIN}.pdf"
-pdflatex ${MAINFILE} > /dev/null && rm -rf ${MAINFILE}.pdf
-pdflatex ${MAINFILE} > /dev/null && mv -f  ${MAINFILE}.pdf ../${DOCMAIN}.pdf
+${PDFTEX} ${MAINFILE} > /dev/null && rm -f ${MAINFILE}.pdf
+${PDFTEX} ${MAINFILE} > /dev/null && mv -f ${MAINFILE}.pdf ../${DOCMAIN}.pdf
 
 echo "- Generating ${DOCHANDOUT}.pdf"
 cat ${MAINFILE}.tex                                                           \
@@ -17,8 +24,8 @@ cat ${MAINFILE}.tex                                                           \
     | sed -e 's/%\\usetheme/\\usetheme/; s/%\\usecolortheme/\\usecolortheme/' \
         > ${HANDOUTFILE}.tex
 
-pdflatex ${HANDOUTFILE} > /dev/null && rm -rf ${HANDOUTFILE}.pdf
-pdflatex ${HANDOUTFILE} > /dev/null && mv -f  ${HANDOUTFILE}.pdf ../${DOCHANDOUT}.pdf
+${PDFTEX} ${HANDOUTFILE} > /dev/null && rm -rf ${HANDOUTFILE}.pdf
+${PDFTEX} ${HANDOUTFILE} > /dev/null && mv -f  ${HANDOUTFILE}.pdf ../${DOCHANDOUT}.pdf
 
 echo "+ Cleaning temporary files"
 rm -rf ${HANDOUTFILE}.tex *.{log,aux,dvi,thm,out,snm,toc,nav,xml}
