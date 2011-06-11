50c7928f825d3fc7449971a21f5845c5f587ba06
Serge Arkhipov <serge@aerialsounds.org>
2011-06-11 20:07:30 +0400

    Some fixes. Dunno which

--- generate_paper.sh
+++ generate_paper.sh
@@ -2,6 +2,7 @@
 
 MAINFILE="main"
 DOCNAME="paper"
+REVNAME="reviews"
 
 PDFVERSION=5
 PDFCOMPRESS=9
@@ -13,10 +14,13 @@ PDFTEX="`which pdflatex` \"${GENOPTS}\""
 cd Paper
 
 echo "- Generating ${DOCNAME}.pdf"
+${PDFTEX} ${REVNAME} > /dev/null && rm -f ${REVNAME}.pdf
+${PDFTEX} ${REVNAME} > /dev/null
+
 ${PDFTEX} ${MAINFILE} > /dev/null && rm -f ${MAINFILE}.pdf
 ${PDFTEX} ${MAINFILE} > /dev/null && mv -f ${MAINFILE}.pdf ../${DOCNAME}.pdf
 
 echo "+ Cleaning temporary files"
-rm -rf *.{dvi,log,aux,thm,toc,out}
+rm -rf *.{dvi,log,aux,thm,toc,out} ${REVNAME}.pdf
 
 echo "Done"
